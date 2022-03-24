380b4db278f9d60edc875443e29ee1208be26867
Alessandro Ghironi <ghironialessandro27@gmail.com>
2022-03-24 19:28:10 +0100

    Sito aggiornato al 24/03/2022

---
+++ (root)/pages/js/loading-animation-game.js
@@ -0,0 +1,36 @@
+
+    var loader = document.querySelector(".loaderGame");
+    window.addEventListener("load", function(){
+        setTimeout(function func(){
+            document.getElementById("noShow").style.display = "none";
+        }, 3000);
+        const loader =  document.querySelector(".loaderGame");
+        loader.className += " hidden";
+        
+    });
+  
+
+
+
+/*
+var invoke_make_blank = false;
+var loader = document.querySelector(".loader");
+
+function make_blank()
+{
+    if (!invoke_make_blank)
+    {
+
+
+            window.addEventListener("load", function(){
+            
+                const loader =  document.querySelector(".loader");
+                loader.className += " hidden";
+            
+                
+            });
+        invoke_make_blank = true;
+    }
+}*/
+
+
