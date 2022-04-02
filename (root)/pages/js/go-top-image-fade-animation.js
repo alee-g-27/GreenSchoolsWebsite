09b7377db69d9d4aa853a8a0be68274180bf759f
Alessandro Ghironi <ghironialessandro27@gmail.com>
2022-04-02 15:57:48 +0200

    Sito Aggiornato alla data 02/04/2022

--- (root)/pages/js/go-top-image-fade-animation.js
+++ (root)/pages/js/go-top-image-fade-animation.js
@@ -1,12 +1,13 @@
 //Get the button:
-mybutton = document.getElementsByClassName(".go-to-top");
+//mybutton = document.getElementsByClassName(".go-top-image");
+/*mybutton = documentElement.getElementsByClassName(".go-top-image");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+  if (document.body.scrollTop < 20 || document.documentElement.scrollTop < 20) {
+    mybutton.style.display = "none";
   } else {
     mybutton.style.display = "none";
   }
@@ -16,4 +17,21 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}*/
+
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 200) {
+      $('.go-top-image').fadeIn();
+  }else {
+      $('.go-top-image').fadeOut();
+  }
+});
+
+function fadeIn(){
+  style.display = "block";
+}
+
+function fadeOut(){
+  style.display = "none";
 }
